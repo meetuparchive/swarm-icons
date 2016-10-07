0b691b45cb1777779a87965e5d0ff25510dfc84d
Adam Detrick <akdetrick@gmail.com>
2016-10-07 13:53:25 -0400

    add initial grunt config for optimization step

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+	grunt.loadNpmTasks('grunt-svgmin');
+	grunt.loadNpmTasks('grunt-svgstore');
+	grunt.loadNpmTasks('grunt-gh-pages');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+
+	var DIST = 'dist/',
+		DIST_OPTIMIZED = DIST + 'optimized/',
+		DIST_ANDROID = DIST + 'android/',
+		DIST_IOS = DIST + 'iOS/',
+		DIST_SPRITE = DIST + 'sprite/';
+
+
+	grunt.initConfig({
+		package: grunt.file.readJSON('package.json'),
+
+		//
+		// CLEAN
+		// removes all distrubtions prior to rebuilding
+		//
+		'clean': {
+			css: [DIST_OPTIMIZED, DIST_ANDROID, DIST_IOS, DIST_SPRITE]
+		},
+
+		//
+		// SVG optimization step
+		// (writes to "optimized" distribution)
+		//
+		'svgmin': {
+			options: {
+				plugins: [
+					{ removeDesc: true },
+					{ collapseGroups: true },
+					{ removeEmptyAttrs: true },
+					{ removeUselessStrokeAndFill: true },
+					{ removeViewbox: false }
+				]
+			},
+			dist: {
+				files: [{
+					expand: true,
+					cwd: 'src/',
+					src: ['**/*.svg'],
+					dest: DIST_OPTIMIZED
+				}]
+			}
+		}
+	});
+
+	grunt.registerTask('dist', ['svgmin', '']);
+	grunt.registerTask('default', ['clean', 'dist']);
+};
