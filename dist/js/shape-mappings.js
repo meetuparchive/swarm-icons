fdfc9e92e422ea3e951b8cd4c385249353d78049
Adam Detrick <akdetrick@gmail.com>
2017-05-26 13:02:20 -0400

    rebuild dist

--- dist/js/shape-mappings.js
+++ dist/js/shape-mappings.js
@@ -10,7 +10,7 @@ const ICON_SHAPE_MAPPINGS = {
 	'arrow-left': 'arrow-right',
 	'chevron-left': 'chevron-right',
 	'chevron-up': 'chevron-right',
-	'chevron-down': 'chevron-right'
+	'chevron-down': 'chevron-right',
 };
 
 export default ICON_SHAPE_MAPPINGS;
