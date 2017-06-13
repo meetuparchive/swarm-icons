8fb7200116e04da86a92bb51888e24b9eb94fab5
Adam Detrick <akdetrick@gmail.com>
2017-06-13 11:11:07 -0400

    diff before build in exportFromSketch script

--- scripts/exportFromSketch.js
+++ scripts/exportFromSketch.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const exportArtboards = require('./util/sketchtoolUtils').exportArtboardsFromFile;
+const exec = require('child_process').exec;
 
 /**
  * Generates icon distributions from sketch files in `src/sketch`
@@ -18,12 +19,35 @@ const DEST_DIR = process.argv[3];
 const PLATFORM = process.argv[4];
 const FORMAT = process.argv[5];
 
-fs.readdirSync(SRC_DIR)
-	.forEach(file => {
-		exportArtboards(
-			`${SRC_DIR}${file}`,
-			DEST_DIR,
-			PLATFORM,
-			FORMAT
-		);
-	});
+/**
+ * @param {Array} modifiedFiles - list of modified files from SRC_DIR
+ */
+const exportFiles = modifiedFiles => {
+	modifiedFiles
+		.forEach(file => {
+			exportArtboards(
+				`${SRC_DIR}${file}`,
+				DEST_DIR,
+				PLATFORM,
+				FORMAT
+			);
+		});
+};
+
+// only build files that have changed
+exec(
+	`git diff master --name-only ${SRC_DIR}`,
+	(error, result) => {
+			if (error !== null) throw new Error(`exec error: ${error}`);
+
+			modifiedFiles = result
+				.split('\n')        // array from stdout lines
+				.filter(f => f)     // filter empty strings
+				.map(f => f         // take only the file name and extension
+					.split(/\//)
+					.pop()
+				);
+
+			exportFiles(modifiedFiles);
+	}
+);
