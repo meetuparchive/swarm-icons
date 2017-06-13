d3b0ce63b590b764488bc35bc4c8b5208e7cf47f
Adam Detrick <akdetrick@gmail.com>
2017-06-13 11:39:12 -0400

    diff _and_ status in export build

--- scripts/exportFromSketch.js
+++ scripts/exportFromSketch.js
@@ -20,10 +20,10 @@ const PLATFORM = process.argv[4];
 const FORMAT = process.argv[5];
 
 /**
- * @param {Array} modifiedFiles - list of modified files from SRC_DIR
+ * @param {Array} fileNames - list of modified files from SRC_DIR
  */
-const exportFiles = modifiedFiles => {
-	modifiedFiles
+const exportFiles = fileNames => {
+	fileNames
 		.forEach(file => {
 			exportArtboards(
 				`${SRC_DIR}${file}`,
@@ -34,25 +34,50 @@ const exportFiles = modifiedFiles => {
 		});
 };
 
+/**
+ * @param {String} stdout - produced by git command
+ * @returns {Array} - array of file names
+ */
+const diffToArray = stdout => stdout
+	.split('\n')        // array from stdout lines
+	.filter(f => f)     // filter empty strings
+	.map(f => f         // take only the file name and extension
+		.split(/\//)
+		.pop()
+	);
+
 // only build files that have changed
+//
+// we can't assume that the sketch files are committed before
+// running the build, so we must `diff` against master _and_
+// `status` the sketch source dir
 exec(
 	`git diff master --name-only ${SRC_DIR}`,
-	(error, result) => {
+	(error, committedFiles) => {
 			if (error !== null) throw new Error(`exec error: ${error}`);
 
-			modifiedFiles = result
-				.split('\n')        // array from stdout lines
-				.filter(f => f)     // filter empty strings
-				.map(f => f         // take only the file name and extension
-					.split(/\//)
-					.pop()
-				);
+			exec(
+				`git status --porcelain ${SRC_DIR}`,
+				(error, localModFiles) => {
+						if (error !== null) throw new Error(`exec error: ${error}`);
 
-			if (modifiedFiles.length) {
-				console.info(`Exporting ${modifiedFiles} as ${FORMAT} for ${PLATFORM}`);
-				exportFiles(modifiedFiles);
-			} else {
-				console.info('No sketch changes found, halting build');
-			}
+					const filesToExport = Array.from(
+						new Set([
+							...diffToArray(committedFiles),
+							...diffToArray(localModFiles)
+						])
+					);
+
+					if (filesToExport.length) {
+						console.info(`Exporting ${filesToExport} as ${FORMAT} for ${PLATFORM}`);
+						exportFiles(filesToExport);
+
+					} else {
+						console.info('No sketch changes found, halting build');
+					}
+
+				}
+			);
+
 	}
 );
